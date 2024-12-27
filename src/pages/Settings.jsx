2f23ada3f2ebf3400ae7c7d16c3c6f2a2f3e3598
karinatimm <kartimm.webdev@gmail.com>
2024-12-27 16:41:49 +0100

    Added Settings.jsx

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,12 @@
+import UpdateSettingsForm from "../features/settings/UpdateSettingsForm";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+
+const Settings = () => (
+  <Row>
+    <Heading as="h1">Update hotel settings</Heading>
+    <UpdateSettingsForm />
+  </Row>
+);
+
+export default Settings;
