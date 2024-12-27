f21f6b4761dd0e1e9640ebc7d768572f5501837c
karinatimm <kartimm.webdev@gmail.com>
2024-12-27 16:36:41 +0100

    Added useOutsideClick.js

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+
+export const useOutsideClick = (closeHandler, listenCapturing = true) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) closeHandler();
+    };
+
+    document.addEventListener("click", handleClick, listenCapturing);
+
+    return () =>
+      document.removeEventListener("click", handleClick, listenCapturing);
+  }, [closeHandler, listenCapturing]);
+
+  return ref;
+};
