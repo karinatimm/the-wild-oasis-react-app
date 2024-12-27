c1afd53348536533d0441684ec48bebcb9c44c09
karinatimm <kartimm.webdev@gmail.com>
2024-12-27 16:22:47 +0100

    Added CabinTable.jsx

---
+++ src/features/cabins/CabinTable.jsx
@@ -0,0 +1,55 @@
+import Spinner from "../../ui/Spinner";
+import CabinRow from "./CabinRow";
+import { useFetchCabinsData } from "./hooks/useFetchCabinsData";
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
+
+const CabinTable = () => {
+  const { isLoading, cabins } = useFetchCabinsData();
+  const [searchParams] = useSearchParams();
+
+  if (isLoading) return <Spinner />;
+  if (cabins.length === 0) return <Empty resourceName={"cabins"} />;
+
+  const filterValue = searchParams.get("discount") || "all";
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  const currentlySelectedSortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = currentlySelectedSortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort((a, b) => {
+    if (typeof a[field] === "string") {
+      return a[field].localeCompare(b[field]) * modifier;
+    }
+    return (a[field] - b[field]) * modifier;
+  });
+
+  return (
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+        <Table.Header role="row">
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
+
+        <Table.Body
+          data={sortedCabins}
+          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+        />
+      </Table>
+    </Menus>
+  );
+};
+
+export default CabinTable;
